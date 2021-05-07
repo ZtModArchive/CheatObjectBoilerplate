8a970e241231317da11e284427a4898d1b5ec4d8
Zt-freak <bnkho98@hotmail.com>
2021-05-07 11:51:32 +0200

    added try-catch with cusom callback

--- scripts/try.lua
+++ scripts/try.lua
@@ -9,3 +9,16 @@ function try(func)
         displayZooMessageTextWithZoom("Exception: "..exception, 1, 30)
     end
 end
+
+--- Function for try-catching with a custom callback
+--- @param func function
+--- @param callback function
+    function tryCatch(func, callback)
+        -- Try
+        local status, exception = pcall(func)
+        -- Catch
+        if not status then
+            -- Call callback function
+            callback(exception)
+        end
+    end
